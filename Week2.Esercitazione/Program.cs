5cdf08ce6152f545a00261803498a37d7f6bc725
Linda Giurca <lindaaa@Lindas-Air.station>
2022-03-11 09:10:04 +0100

    first commit

---
+++ Week2.Esercitazione/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Week2.Esercitazione
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
